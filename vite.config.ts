1350bdcfb3d04e1e4c6b7cdf125dfd4b5c45f989
Richard Ockerby <richard@arjo.dev>
2026-07-15 09:44:08 +0100

    Fix comma in the config file

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Devmon-Game/'
+    base: '/Devmon-Game/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
